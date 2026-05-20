5f858417a2f854ef38cea635714473d59ddd457a
FightingKazuo <kkazushi10032001@gmail.com>
2026-05-20 12:13:45 +0900

    service-worker.js を更新

--- public/public/service-worker.js
+++ public/public/service-worker.js
@@ -1,16 +1,39 @@
-const CACHE_NAME = 'timeflow-v1';
-const urlsToCache = ['/'];
+const CACHE_NAME = 'timeflow-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/bundle.js',
+  '/static/js/vendors~main.chunk.js',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json',
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      }).catch(() => cached);
     })
   );
 });
